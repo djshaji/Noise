ec0473ddec699999cc9da119ba81698009ea8b0b
Shaji Khan <git@shaji.in>
2022-02-20 17:28:39 +0530

    compiles.. and if it compiles it runs, right? RIGHT??

--- app/src/main/java/com/shajikhan/ladspa/noise/MainActivity.java
+++ app/src/main/java/com/shajikhan/ladspa/noise/MainActivity.java
@@ -1,13 +1,32 @@
 package com.shajikhan.ladspa.noise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shajikhan.ladspa.noise.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback{
+    private static final String TAG = MainActivity.class.getName();
+    private static final int AUDIO_EFFECT_REQUEST = 0;
+    private static final int OBOE_API_AAUDIO = 0;
+    private static final int OBOE_API_OPENSL_ES=1;
+    private Button toggleEffectButton;
+    private boolean isPlaying = false;
+
+    private int apiSelection = OBOE_API_AAUDIO;
+    private boolean mAAudioRecommended = true;
+
+
 
     // Used to load the 'noise' library on application startup.
     static {
@@ -26,6 +45,84 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+    }
+
+    public void toggleEffect() {
+        if (isPlaying) {
+            stopEffect();
+        } else {
+            AudioEngine.setAPI(apiSelection);
+            startEffect();
+        }
+    }
+
+    private void startEffect() {
+        Log.d(TAG, "Attempting to start");
+
+        if (!isRecordPermissionGranted()){
+            requestRecordPermission();
+            return;
+        }
+
+        boolean success = AudioEngine.setEffectOn(true);
+        if (success) {
+            isPlaying = true;
+        } else {
+            isPlaying = false;
+        }
+    }
+
+    private void stopEffect() {
+        Log.d(TAG, "Playing, attempting to stop");
+        AudioEngine.setEffectOn(false);
+        toggleEffectButton.setText("Start");
+        isPlaying = false;
+    }
+    /*
+    private int getRecordingDeviceId(){
+        return ((AudioDeviceListEntry)recordingDeviceSpinner.getSelectedItem()).getId();
+    }
+
+    private int getPlaybackDeviceId(){
+        return ((AudioDeviceListEntry)playbackDeviceSpinner.getSelectedItem()).getId();
+    }
+
+     */
+
+    private boolean isRecordPermissionGranted() {
+        return (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) ==
+                PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestRecordPermission(){
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                AUDIO_EFFECT_REQUEST);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (AUDIO_EFFECT_REQUEST != requestCode) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 1 ||
+                grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+            // User denied the permission, without this we cannot record audio
+            // Show a toast and update the status accordingly
+            Toast.makeText(getApplicationContext(),
+                    "User permission denied!",
+                    Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            // Permission was granted, start live effect
+            toggleEffect();
+        }
     }
 
     /**
